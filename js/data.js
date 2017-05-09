97e43982a0a156c11c500843c188562cef5d8d81
Szász Tibor <tszasz@gmail.com>
2017-05-09 13:52:56 +0200

    feat(gallery): added image data

---
+++ js/data.js
@@ -0,0 +1,28 @@
+
+const images = [
+	{
+		title: 'Tower',
+		desc: 'lorem dflkbndoli zhré teyrotiewh4 tosyhrtpoiisyhe',
+		file: '1.jpg'
+	},
+	{
+		title: 'City',
+		desc: 'ming of the Sun’s historical passages through the Galaxy’s spiral arms. ',
+		file: '2.jpg'
+	},
+	{
+		title: 'Cat I',
+		desc: 'Mass extinctions have the effect of wiping the biological slate clean, freeing up ecological niches and thus producing explosions in biodiversit',
+		file: '3.jpg'
+	},
+	{
+		title: 'Cat II',
+		desc: 'Nope! Nope! Nope!',
+		file: '4.jpg'
+	},
+	{
+		title: 'BOKEH',
+		desc: '',
+		file: '5.jpg'
+	}
+]
